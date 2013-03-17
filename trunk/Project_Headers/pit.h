60e07a38a39d2d6cb16245e59a5c305d4476deaa
maserra3@asu.edu <maserra3@asu.edu@f6e53067-18ad-9412-d7fb-9032d478e719>
2013-03-17 08:06:23 +0000

    gdh

--- trunk/Project_Headers/pit.h
+++ trunk/Project_Headers/pit.h
@@ -8,5 +8,12 @@
 #ifndef PIT_H_
 #define PIT_H_
 
+#include "support_common.h"
+#include "interupt.h"
+
+extern void pit0_init();
+extern void set_note_length(int, int);
+extern __declspec(interrupt) void pit0_isr();
+extern void set_tempo(int);
 
 #endif /* PIT_H_ */

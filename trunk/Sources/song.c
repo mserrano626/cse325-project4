0bec3709ced0f0820b8437b6c2038f55d7a3deb9
maserra3@asu.edu <maserra3@asu.edu@f6e53067-18ad-9412-d7fb-9032d478e719>
2013-03-17 08:05:11 +0000

    fdsg

--- trunk/Sources/song.c
+++ trunk/Sources/song.c
@@ -7,6 +7,7 @@
 
 #include "note.h"
 #include "song.h"
+#include "pit.h"
 /*for testing*/
 static void busy_delay(int p_delay)
 {
@@ -19,31 +20,91 @@ static void busy_delay(int p_delay)
 
 /*test song*/
 void play_song(){
-	note(0x00);
-			busy_delay(500000);
-			note(0x01);
-			busy_delay(500000);
-			note(0x02);
-			busy_delay(500000);
-			note(0x03);
-			busy_delay(500000);
-			note(0x04);
-			busy_delay(500000);
-			note(0x05);
-			busy_delay(500000);
-			note(0x06);
-			busy_delay(500000);
-			note(0x07);
-			busy_delay(500000);
-			note(0x08);
-			busy_delay(500000);
-			note(0x09);
-			busy_delay(500000);
-			note(0x0A);
-			busy_delay(500000);
-			note(0x0B);
-			busy_delay(500000);
-			note(0x0D);
-			busy_delay(500000);
+			
+			/*
+			note(0x06,0);
+			busy_delay(1000000);
+			*/
+			busy_delay(1000000);
+			note(0x01,0x00);//c
+			
+			busy_delay(100);
+			note(0xFF,0);
+			
+			busy_delay(1000000);
+			note(0x01,0x00);//c
+			busy_delay(1000000);
+			
+			note(0x08,0x00);//g
+			busy_delay(1000000);
+			
+			busy_delay(100);
+			note(0xFF,0);
+			
+			note(0x08,0);
+			busy_delay(1000000);
+			
+			note(0x0A,0);
+			busy_delay(1000000);
+			
+			busy_delay(100);
+			note(0xFF,0);
+			
+			note(0x0A,0);
+			busy_delay(1000000);
+			
+			note(0x08,0);
+			busy_delay(1000000*2);
+			/*
+			note(0x06,0);
+			busy_delay(1000000);
+			note(0x07,0);
+			busy_delay(1000000);
+			note(0x08,0);
+			busy_delay(1000000);
+			note(0x09,0);
+			busy_delay(1000000);
+			note(0x0A,0);
+			busy_delay(1000000);
+			note(0x0B,0);
+			busy_delay(1000000);
+			note(0x0D,0);
+			busy_delay(1000000);
+			*/
+			
+}
+
+void song_notes(){
 	
+	
+	note(0x00,0x00);
+	//busy_delay(1000000);
+	//pit0_isr();
+	note(0x01,0x01);
+	//busy_delay(1000000);
+	note(0x02,0x03);
+	//pit0_isr();
+	//busy_delay(1000000);
+	note(0x03,3);
+	//pit0_isr();
+	
+	//busy_delay(1000000);
+	note(0x04,5);
+	//busy_delay(1000000);
+	note(0x05,5);
+	//busy_delay(1000000);
+	note(0x06,5);
+	//busy_dely(1000000);
+	note(0x07,5);
+	//busy_delay(1000000);
+	note(0x08,3);
+	//busy_delay(1000000);
+	note(0x09,3);
+	//busy_delay(1000000);
+	note(0x0A,1);
+	//busy_delay(1000000);
+	note(0x0B,0);
+	//busy_delay(1000000);
+	note(0x0D,0);
+	//busy_delay(1000000);
 }

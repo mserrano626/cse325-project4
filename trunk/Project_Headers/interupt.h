37fcf5420935495e31e628598300046ec177f797
maserra3@asu.edu <maserra3@asu.edu@f6e53067-18ad-9412-d7fb-9032d478e719>
2013-03-17 08:05:42 +0000

    gdh

--- trunk/Project_Headers/interupt.h
+++ trunk/Project_Headers/interupt.h
@@ -8,5 +8,12 @@
 #ifndef INTERUPT_H_
 #define INTERUPT_H_
 
+#include "support_common.h"
+
+typedef void (*isr_funct)(); //isr_funct is a type which is a pointer to a void function with no params
+
+extern void interupt_config(int src, int level, int priority, isr_funct isr);
+extern void interupt_unmask_src(int src);
+extern asm __declspec(standard_abi) void int_uninhibit_all();
 
 #endif /* INTERUPT_H_ */
